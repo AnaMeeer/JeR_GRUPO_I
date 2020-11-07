e3c043f534230e6b1ac0a6e85d0ed4d6d9ee04ba
MiguelVDP <66267150+MiguelVDP@users.noreply.github.com>
2020-11-07 13:03:17 +0100

    liada padre
    que la he liado bro

--- src/scenes/EscenaPrueba.js
+++
@@ -1,9 +0,0 @@
-export default class EscenaPrueba extends Phaser.Scene {
-    constructor() {
-        super({ key: "EscenaPrueba" });
-    }
-
-    create(){
-        console.log("Se ha creado la escena de prueba");
-    }
-}
